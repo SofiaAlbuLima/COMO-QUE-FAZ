4eb06ff4271c95b38e5ec5fcdfb88481082f0d22
Your Name <you@example.com>
2024-04-03 09:08:31 -0300

    include - template header e footer feito e incluido na index

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+
+router.get("/", function (req, res) {
+    res.render("pages/template-home", {pagina:"index", logado:null});
+});
+
+
+
 var fabricaDeConexao = require("../../config/connection_factory");
 var conexao = fabricaDeConexao
 
