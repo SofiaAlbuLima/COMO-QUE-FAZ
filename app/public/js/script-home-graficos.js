8d5a73b1cc8724322903a63acf8fefd4a5155ef2
Sofia <albuquerque.sofialima@gmail.com>
2024-04-29 21:33:40 -0300

    tabelas administrativas - inicio 4

--- app/public/js/script-home-graficos.js
+++ app/public/js/script-home-graficos.js
@@ -1,6 +1,17 @@
-const xValues = ["Denúncias analisadas e avaliadas", "Denúncias Não analisadas", "Denúncias analisadas"];
 const yValues = [100, 50, 50];
+
+const totalVal = yValues.reduce((acc, current) => acc + current, 0);
+const Val = [ (yValues[0] * 100) / totalVal,
+              (yValues[1] * 100) / totalVal,
+              (yValues[2] * 100) / totalVal,
+];
+
+const xValues = [ Val[0] + "% Denúncias analisadas e avaliadas", 
+                  Val[1] + "% Denúncias Não analisadas", 
+                  Val[2] + "% Denúncias analisadas"];
 const barColors = ["#5800AA", "#AA007A","#FFD662"];
+
+
 
 new Chart("Grafico_Denuncias", {
     type: "doughnut",
@@ -12,19 +23,18 @@ new Chart("Grafico_Denuncias", {
         }]
     },
     options: {
-        cutoutPercentage: 60,
+        maintainAspectRatio: false, // Esta opção permite ao gráfico ignorar o aspect ratio e se ajustar ao tamanho do contêiner
+        responsive: false,
         legend: {
           position: 'right',
           labels: {
-              fontColor: '#333333',
-              fontSize: 14,
+              fontColor: 'rgba(51, 51, 51, 100)',
+              fontSize: 18,
               fontFamily: 'Fira Sans',
+              padding: 30,
               boxWidth: 20,
-              marginBottom: 80
-          },
-          margin: {
-            bottom: 60 // Definindo a margem de 20 pixels abaixo da legenda
-        }
+              boxHeight: 20,
+          }
       }
     }
   });
