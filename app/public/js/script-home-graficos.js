97cb52f99f1e7fc6857f4858ca6ac2cff4498f90
Sofia <albuquerque.sofialima@gmail.com>
2024-05-01 22:31:37 -0300

    Marketing & Banners - inicio 5

--- app/public/js/script-home-graficos.js
+++ app/public/js/script-home-graficos.js
@@ -1,35 +1,34 @@
-const yValues = [100, 50, 50];
+const yDenuncias = [100, 50, 50];
 
-const totalVal = yValues.reduce((acc, current) => acc + current, 0);
-const Val = [ (yValues[0] * 100) / totalVal,
-              (yValues[1] * 100) / totalVal,
-              (yValues[2] * 100) / totalVal,
+const totalVal = yDenuncias.reduce((acc, current) => acc + current, 0);
+const Val = [ (yDenuncias[0] * 100) / totalVal,
+              (yDenuncias[1] * 100) / totalVal,
+              (yDenuncias[2] * 100) / totalVal,
 ];
 
-const xValues = [ Val[0] + "% Denúncias analisadas e avaliadas", 
-                  Val[1] + "% Denúncias Não analisadas", 
-                  Val[2] + "% Denúncias analisadas"];
-const barColors = ["#5800AA", "#AA007A","#FFD662"];
+const xDenuncias = [ Val[0] + "% Denúncias analisadas e avaliadas", 
+                     Val[1] + "% Denúncias Não analisadas", 
+                     Val[2] + "% Denúncias analisadas"];
+const barColors = ["#5800AA", "#AA007A","#FFD662", "#333333"];
 
 
 
 new Chart("Grafico_Denuncias", {
     type: "doughnut",
     data: {
-        labels: xValues,
+        labels: xDenuncias,
         datasets: [{
           backgroundColor: barColors,
-          data: yValues,
+          data: yDenuncias,
         }]
     },
     options: {
-        maintainAspectRatio: false, // Esta opção permite ao gráfico ignorar o aspect ratio e se ajustar ao tamanho do contêiner
+        maintainAspectRatio: false,
         responsive: false,
         legend: {
           position: 'right',
           labels: {
-              fontColor: 'rgba(51, 51, 51, 100)',
-              fontSize: 18,
+              fontSize: 15,
               fontFamily: 'Fira Sans',
               padding: 30,
               boxWidth: 20,
@@ -40,3 +39,52 @@ new Chart("Grafico_Denuncias", {
   });
 
 //   https://www.w3schools.com/js/js_graphics_chartjs.asp
+
+
+const LabelPostagens = [500, 345, 200];
+const LabelPerguntas = [250, 100, 50];
+
+new Chart("Grafico_Postagens", {
+  type: 'bar',
+    data: {
+        labels: ['Culinária', 'Limpeza', 'Bem Estar'],
+        datasets: [{
+            label: 'Quantidade de Postagens',
+            data: LabelPostagens,
+            backgroundColor: barColors[0] 
+        }, {
+            label: 'Quantidade de Perguntas',
+            data: LabelPerguntas,
+            backgroundColor: barColors[1]
+        }]
+    },
+    options: {
+      maintainAspectRatio: false,
+      responsive: false,
+        legend: {
+          labels: {
+              fontSize: 15,
+              fontFamily: 'Fira Sans',
+              boxWidth: 20,
+              boxHeight: 20,
+              padding: 10
+          }
+      },
+        plugins: {
+            datalabels: {
+                anchor: 'end',
+                align: 'end'
+            }
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        },
+        // Ajuste a largura das barras aqui
+        barPercentage: 0.5,
+        categoryPercentage: 0.5
+    }
+});
