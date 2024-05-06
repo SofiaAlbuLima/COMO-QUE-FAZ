1fd599186f9621bc1f5f7fff46896c9fb913ebdf
Sofia <albuquerque.sofialima@gmail.com>
2024-05-06 09:56:34 -0300

    Denuncias - formatação das celulas da tabela

---
+++ app/public/js/script-adm-tabelas.js
@@ -0,0 +1,141 @@
+function myFunction() {
+  var input, //o campo de entrada onde o usuário digita o filtro
+      filter, //o valor do filtro
+      table, //a tabela que será filtrada
+      tr, // representa as linhas da tabela
+      td, //representa as células da tabela
+      i, //contador usado no loop
+      txtValue; //valor do texto dentro de cada célula da tabela
+      
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[4];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    FuncaoCelulas();
+});
+
+function FuncaoCelulas() {
+  var table = document.getElementById("myTable");
+  var tr = table.getElementsByTagName("tr");
+
+  console.log("funcaoo");
+  for (i = 0; i < tr.length; i++) {
+    var cells = tr[i].getElementsByTagName("td"); // Obtém todas as células da linha
+    
+    for (var j = 0; j < cells.length; j++) { // Percorre todas as células da linha
+        var cell = cells[j];
+        txtValue = cell.textContent || cell.innerText;
+        
+        // Exemplo de lógica para atribuir classes e IDs com base no conteúdo da célula
+        if (txtValue === "Perfil") {
+            cell.classList.add("Conteudo");
+            var texto = document.createElement("p");
+            var figure = document.createElement("figure");
+
+            texto.innerHTML = "Perfil";
+            texto.classList.add("p-perfil", "p-conteudo");
+            cell.appendChild(texto);
+
+            figure.classList.add("figure-perfil", "figure-conteudo");
+            cell.appendChild(figure);
+
+        } else if (txtValue === "Pergunta") {
+          cell.classList.add("Conteudo");
+          var texto = document.createElement("p");
+          var figure = document.createElement("figure");
+
+          texto.innerHTML = "Pergunta";
+          texto.classList.add("p-pergunta", "p-conteudo");
+          cell.appendChild(texto);
+
+          figure.classList.add("figure-pergunta", "figure-conteudo");
+          cell.appendChild(figure);
+
+
+        } else if (txtValue === "Postagem") {
+          cell.classList.add("Conteudo");
+          var texto = document.createElement("p");
+          var figure = document.createElement("figure");
+
+          texto.innerHTML = "Postagem";
+          texto.classList.add("p-postagem", "p-conteudo");
+          cell.appendChild(texto);
+
+          figure.classList.add("figure-postagem", "figure-conteudo");
+          cell.appendChild(figure);
+
+
+        } else if (txtValue === "Comentário") {
+          cell.classList.add("Conteudo");
+          var texto = document.createElement("p");
+          var figure = document.createElement("figure");
+
+          texto.innerHTML = "Comentário";
+          texto.classList.add("p-conteudo", "p-comentario");
+          cell.appendChild(texto);
+
+          figure.classList.add("figure-conteudo", "figure-comentario");
+          cell.appendChild(figure);
+
+
+        } else if (txtValue === "Analisada e Avaliada") {
+              cell.classList.add("Status");
+              var texto = document.createElement("p");
+              var figure = document.createElement("figure");
+
+              texto.innerHTML = "Analisada e Avaliada";
+              texto.classList.add("p-analisadaAvaliada", "p-status");
+              cell.appendChild(texto);
+
+              figure.classList.add("figure-analisadaAvaliada", "figure-status");
+              cell.appendChild(figure);
+
+
+          } else if (txtValue === "Não Analisada") {
+            cell.classList.add("Status");
+            var texto = document.createElement("p");
+            var figure = document.createElement("figure");
+
+            texto.innerHTML = "Não Analisada";
+            texto.classList.add("p-naoAnalisada", "p-status");
+            cell.appendChild(texto);
+
+            figure.classList.add("figure-naoAnalisada", "figure-status");
+            cell.appendChild(figure);
+
+
+          } else if (txtValue === "Analisada") {
+            cell.classList.add("Status");
+            var texto = document.createElement("p");
+            var figure = document.createElement("figure");
+
+            texto.innerHTML = "Analisada";
+            texto.classList.add("p-analisada", "p-status");
+            cell.appendChild(texto);
+
+            figure.classList.add("figure-analisada", "figure-status");
+            cell.appendChild(figure);
+
+
+          } else {
+                // Caso padrão, se não corresponder a nenhum critério específico
+                cell.classList.add("classe-padrao");
+            }
+    }
+}
+}
--- app/public/js/script-home-graficos.js
+++ app/public/js/script-home-graficos.js
@@ -1,90 +1,190 @@
-const yDenuncias = [100, 50, 50];
 
-const totalVal = yDenuncias.reduce((acc, current) => acc + current, 0);
-const Val = [ (yDenuncias[0] * 100) / totalVal,
-              (yDenuncias[1] * 100) / totalVal,
-              (yDenuncias[2] * 100) / totalVal,
-];
+//   Gráfico - Denúncias | Análises e avaliação - Inicio
+    const yDenuncias = [100, 50, 50];
 
-const xDenuncias = [ Val[0] + "% Denúncias analisadas e avaliadas", 
-                     Val[1] + "% Denúncias Não analisadas", 
-                     Val[2] + "% Denúncias analisadas"];
-const barColors = ["#5800AA", "#AA007A","#FFD662", "#333333"];
+    const totalVal = yDenuncias.reduce((acc, current) => acc + current, 0);
+    const Val = [ (yDenuncias[0] * 100) / totalVal,
+                (yDenuncias[1] * 100) / totalVal,
+                (yDenuncias[2] * 100) / totalVal,
+    ];
 
+    const xDenuncias = [ Val[0] + "% Denúncias analisadas e avaliadas", 
+                        Val[1] + "% Denúncias Não analisadas", 
+                        Val[2] + "% Denúncias analisadas"];
+    const barColors = ["#5800AA", "#AA007A","#FFD662", "#333333"];
 
-
-new Chart("Grafico_Denuncias", {
-    type: "doughnut",
-    data: {
-        labels: xDenuncias,
-        datasets: [{
-          backgroundColor: barColors,
-          data: yDenuncias,
-        }]
-    },
-    options: {
-        maintainAspectRatio: false,
-        responsive: false,
-        legend: {
-          position: 'right',
-          labels: {
-              fontSize: 15,
-              fontFamily: 'Fira Sans',
-              padding: 30,
-              boxWidth: 20,
-              boxHeight: 20,
-          }
-      }
-    }
-  });
-
-//   https://www.w3schools.com/js/js_graphics_chartjs.asp
-
-
-const LabelPostagens = [500, 345, 200];
-const LabelPerguntas = [250, 100, 50];
-
-new Chart("Grafico_Postagens", {
-  type: 'bar',
-    data: {
-        labels: ['Culinária', 'Limpeza', 'Bem Estar'],
-        datasets: [{
-            label: 'Quantidade de Postagens',
-            data: LabelPostagens,
-            backgroundColor: barColors[0] 
-        }, {
-            label: 'Quantidade de Perguntas',
-            data: LabelPerguntas,
-            backgroundColor: barColors[1]
-        }]
-    },
-    options: {
-      maintainAspectRatio: false,
-      responsive: false,
-        legend: {
-          labels: {
-              fontSize: 15,
-              fontFamily: 'Fira Sans',
-              boxWidth: 20,
-              boxHeight: 20,
-              padding: 10
-          }
-      },
-        plugins: {
-            datalabels: {
-                anchor: 'end',
-                align: 'end'
-            }
-        },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
+    new Chart("Grafico_Denuncias", {
+        type: "doughnut",
+        data: {
+            labels: xDenuncias,
+            datasets: [{
+            backgroundColor: barColors,
+            data: yDenuncias,
             }]
         },
-        // Ajuste a largura das barras aqui
-        barPercentage: 0.5,
-        categoryPercentage: 0.5
-    }
-});
+        options: {
+            maintainAspectRatio: false,
+            responsive: false,
+            legend: {
+            position: 'right',
+            labels: {
+                fontSize: 15,
+                fontFamily: 'Fira Sans',
+                padding: 30,
+                boxWidth: 20,
+                boxHeight: 20,
+            }
+        }
+        }
+    });
+//   Gráfico - Denúncias | Análises e avaliação - Fim
+
+
+
+//   Gráfico - Postagens & Perguntas | Publicações de Categorias - Inicio
+    const LabelPostagens = [500, 345, 200];
+    const LabelPerguntas = [250, 100, 50];
+
+    new Chart("Grafico_Postagens", {
+    type: 'bar',
+        data: {
+            labels: ['Culinária', 'Limpeza', 'Bem Estar'],
+            datasets: [{
+                label: 'Quantidade de Postagens',
+                data: LabelPostagens,
+                backgroundColor: barColors[0] 
+            }, {
+                label: 'Quantidade de Perguntas',
+                data: LabelPerguntas,
+                backgroundColor: barColors[1]
+            }]
+        },
+        options: {
+        maintainAspectRatio: false,
+        responsive: false,
+            legend: {
+            labels: {
+                fontSize: 15,
+                fontFamily: 'Fira Sans',
+                boxWidth: 20,
+                boxHeight: 20,
+                padding: 10
+            }
+        },
+            plugins: {
+                datalabels: {
+                    anchor: 'end',
+                    align: 'end'
+                }
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true,
+                        stepSize: 100
+                    }
+                }]
+            },
+            // Ajuste a largura das barras aqui
+            barPercentage: 0.5,
+            categoryPercentage: 0.5
+        }
+    });
+//   Gráfico - Postagens & Perguntas | Publicações de Categorias - Fim
+
+
+
+//   Gráfico - Acesso Premium - Inicio
+    const yPremium = [500, 2500];
+    const xPremium = [yPremium[0] + " Usuários Premium", yPremium[1] +" Usuários sem Premium"]
+    const barColorsPremium = ["#FFD662", "#5800AA"];
+
+    new Chart("Grafico_AcessoPremium", {
+        type: "doughnut",
+        data: {
+            labels: xPremium,
+            datasets: [{
+            backgroundColor: barColorsPremium,
+            data: yPremium,
+            }]
+        },
+        options: {
+            maintainAspectRatio: false,
+            responsive: false,
+            legend: {
+            position: 'bottom',
+            labels: {
+                fontSize: 16,
+                fontFamily: 'Fira Sans',
+                padding: 30,
+                boxWidth: 20,
+                boxHeight: 20,
+            }
+        }
+        }
+    });
+//   Gráfico - Acesso Premium - Fim
+
+
+
+//   Gráfico - Acesso Premium | Cadastros e Acessos - Inicio
+    const yCadastros = [0, 100, 100, 50, 150, 0];
+    const yPermanêcia = [125, 50, 150, 150, 100, 100];
+
+    const LabelDatas = ['01 Jan 2024', '01 Fev 2024', '01 Mar 2024', '01 Abr 2024', '01 Mai 2024', '01 Jun 2024',];
+
+    const xCadastros = ["Novos cadastros"];
+    const xPermanêcia = ["Duração média de permanência"];
+
+    new Chart("Grafico_CadastrosAcessos", {
+        type: 'line',
+            data: {
+                labels: LabelDatas,
+                datasets: [{
+                    label: xCadastros,
+                    data: yCadastros,
+                    backgroundColor: 'transparent',
+                    borderColor: barColors[1],
+                    tension: 0
+                }, {
+                    label: xPermanêcia,
+                    data: yPermanêcia,
+                    backgroundColor: 'transparent',
+                    borderColor: barColors[2],
+                    tension: 0
+                }]
+            },
+            options: {
+            maintainAspectRatio: false,
+            responsive: false,
+                legend: {
+                    position: 'right',
+                labels: {
+                    fontSize: 16,
+                    fontFamily: 'Fira Sans',
+                    boxWidth: 20,
+                    boxHeight: 20,
+                    padding: 30
+                }
+            },
+                plugins: {
+                    datalabels: {
+                        anchor: 'end',
+                        align: 'end'
+                    }
+                },
+                scales: {   
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true,
+                            stepSize: 50
+                        }
+                    }]
+                },
+                // Ajuste a largura das barras aqui
+                barPercentage: 0.5,
+                categoryPercentage: 0.5
+            }
+        });
+
+//   Gráfico - Acesso Premium | Cadastros e Acessos - Fim
