f9d99797f02fa3e2e5a733a5f7d41949861efcba
Sofia <albuquerque.sofialima@gmail.com>
2024-05-08 22:44:41 -0300

    links pagina inicial, login e adm

--- app/routes/router-link.js
+++ app/routes/router-link.js
@@ -3,45 +3,6 @@ var router = express.Router();
 
 
 // Links Páginas Administrativas - Inicio
-router.get("/adm/administrativa", function (req, res) {
-    res.render("pages/template-adm", {pagina: {cabecalho: "menu-administrativo", conteudo: "administrativa"}, logado:null});
-});
-router.get('/adm/adm-denuncias', function(req, res){
-    res.render('partial/administrar/adm-denuncias.ejs'); 
-});
-router.get('/adm/adm/adm-postagens', function(req, res){
-    res.render('partial/administrar/adm-postagens.ejs'); 
-});
-router.get('/adm/adm-usuarios', function(req, res){
-    res.render('partial/administrar/adm-usuarios.ejs'); 
-});
-router.get('/adm/adm-premium', function(req, res){
-    res.render('partial/administrar/adm-premium.ejs'); 
-});
-router.get('/adm/adm-banners', function(req, res){
-    res.render('partial/administrar/adm-banners.ejs'); 
-});
-// Links Páginas Administrativas - Fim
 
-// Template do menu administrativo - Inicio
-router.get("/administrativa", function (req, res) {
-    res.render("partial/menu-administrativo", {pagina:"administrativa", logado:null});
-});
-router.get("/adm-denuncias", function (req, res) {
-    res.render("partial/menu-administrativo", {pagina:"adm-denuncias", logado:null});
-});
-router.get("/adm-postagens", function (req, res) {
-    res.render("partial/menu-administrativo", {pagina:"adm-postagens", logado:null});
-});
-router.get("/adm-usuarios", function (req, res) {
-    res.render("partial/menu-administrativo", {pagina:"adm-usuarios", logado:null});
-});
-router.get("/adm-premium", function (req, res) {
-    res.render("partial/menu-administrativo", {pagina:"adm-premium", logado:null});
-});
-router.get("/adm-banners", function (req, res) {
-    res.render("partial/menu-administrativo", {pagina:"adm-banners", logado:null});
-});
-// Template do menu administrativo - Fim
 
 module.exports = router;
--- app/routes/router.js
+++ app/routes/router.js
@@ -1,19 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-
+// Links & Template - Parte Publica
 router.get("/", function (req, res) {
-    res.render("pages/template", {pagina: {cabecalho : "cabecalho", conteudo: "index",rodape: "rodape"}, logado:null});
+    res.render("pages/template", {pagina: {cabecalho: "cabecalho", conteudo: "index", rodape: "rodape"}, logado:null});
 });
+    router.get("/login", function (req, res) {
+        res.render("pages/template", {
+            pagina: {cabecalho: "cabecalho", conteudo: "Fazer-Login", rodape: "rodape"}, logado:null});
+    });
+
+// Links & Template - Parte Administrativa
+router.get("/adm", function (req, res) {
+    res.render("pages/template-adm", {pagina: {cabecalho: "administrar/menu-administrativo", conteudo: "administrar/paginas/administrativa"}, logado:null});
+});
+    router.get("/adm/denuncias", function (req, res) {
+        res.render("pages/template-adm", {pagina: {cabecalho: "administrar/menu-administrativo", conteudo: "administrar/paginas/adm-denuncias"}, logado:null});
+    });
+    router.get("/adm/postagens-perguntas", function (req, res) {
+        res.render("pages/template-adm", {pagina: {cabecalho: "administrar/menu-administrativo", conteudo: "administrar/paginas/adm-postagens"}, logado:null});
+    });
+    router.get("/adm/usuarios", function (req, res) {
+        res.render("pages/template-adm", {pagina: {cabecalho: "administrar/menu-administrativo", conteudo: "administrar/paginas/adm-usuarios"}, logado:null});
+    });
+    router.get("/adm/acesso-premium", function (req, res) {
+        res.render("pages/template-adm", {pagina: {cabecalho: "administrar/menu-administrativo", conteudo: "administrar/paginas/adm-premium"}, logado:null});
+    });
+    router.get("/adm/marketing-banners", function (req, res) {
+        res.render("pages/template-adm", {pagina: {cabecalho: "administrar/menu-administrativo", conteudo: "administrar/paginas/adm-banners"}, logado:null});
+    });
 
 
 var fabricaDeConexao = require("../../config/connection_factory");
-var conexao = fabricaDeConexao
-
-router.get('/', function(req, res){
-    res.render('partial/index');
-});
-
+var conexao = fabricaDeConexao;
 
 
 module.exports = router;
