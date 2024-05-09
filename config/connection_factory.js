5307c93ca09e6bf0dbdd61fa2f4f14c387bf7784
Your Name <you@example.com>
2024-05-09 08:58:34 -0300

    uhull

--- config/connection_factory.js
+++ config/connection_factory.js
@@ -1,22 +1,22 @@
-var mysql = require("mysql2");
-const dotenv = require("dotenv");
-dotenv.config();
+// var mysql = require("mysql2");
+// const dotenv = require("dotenv");
+// dotenv.config();
 
-module.exports = function(){
-    try{
-        let conexao = mysql.createConnection({
-            host: process.env.HOST,
-            user: process.env.USER,
-            password: process.env.PASSWORDITB,
-            database: process.env.DATABASE,
-            port: process.env.PORT
-        });
-        console.log("Conexão estabelecida!");
-        return conexao;
+// module.exports = function(){
+//     try{
+//         let conexao = mysql.createConnection({
+//             host: process.env.HOST,
+//             user: process.env.USER,
+//             password: process.env.PASSWORDITB,
+//             database: process.env.DATABASE,
+//             port: process.env.PORT
+//         });
+//         console.log("Conexão estabelecida!");
+//         return conexao;
         
-    } catch (e){
-        console.log("Falha ao estabelecer a conexão");
-        console.log(e);
-        return null
-    }
-}
+//     } catch (e){
+//         console.log("Falha ao estabelecer a conexão");
+//         console.log(e);
+//         return null
+//     }
+// }
---
+++ config/pool-conexoes.js
@@ -0,0 +1,27 @@
+// const mysql = require('mysql2');
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     // database: process.env.,
+//     port: process.env.DB_PORT,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
+// })
+
+// pool.getConnection((err, conn) => {
+//     // try {
+//     //     const [linhas,campos] = await pool.query('SELECT * FROM tabela')
+//     // } catch(erro){
+//     //     console.log(erro)
+//     // }
+//     if(err){
+//         console.log(err)
+//     }
+//     else{
+//         console.log("Conecatado ao SGBD!")
+//     }
+// })
+
+// module.exports = pool.promise()
