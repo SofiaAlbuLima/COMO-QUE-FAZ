cb49efa3bec5b4ab3b21dbf3cb0d829404bb9289
Your Name <you@example.com>
2024-05-27 11:00:27 -0300

    uai

---
+++ app/controllers/controller.js
@@ -0,0 +1,7 @@
+// regrasValidacao: [
+//     body("tarefa")
+//       .isLength({ min: 5, max: 45 })
+//       .withMessage("Nome da Tarefa deve conter de 5 a 45 letras!"),
+//     body("prazo").isISO8601(),
+//     body("situacao").isNumeric(),
+//   ],
