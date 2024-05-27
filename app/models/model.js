f152369ff2f575538fd627248a107ce2ee5dcecf
Your Name <you@example.com>
2024-05-27 11:00:34 -0300

    model

--- app/models/model.js
+++ app/models/model.js
@@ -3,7 +3,7 @@ const pool = require("../../config/pool-conexoes");
 const clienteModel = {
     findAll: async()=>{
         try {
-            const [linhas,campos] = await pool.query('SELECT * FROM clientes')
+            const [linhas, campos] = await pool.query('SELECT * FROM clientes')
             return linhas
         } catch(erro){
             console.log(erro)
