ff6720e9cdfd3a253524e3a0b13cda3cb4cc99f1
Sofia <albuquerque.sofialima@gmail.com>
2024-06-14 21:48:50 -0300

    remoção de arquivos inuteis

--- app/public/js/script-header.js
+++ app/public/js/script-header.js
@@ -10,11 +10,3 @@
             document.getElementById("text-user-hover").style.color = "#333333";
             document.getElementById("text-user-hover2").style.color = "#333333";
         }
-
-        function showDiv2() {
-            document.getElementById("hide-hover-search").style.display = "block";
-        }
-
-        function hideDiv2() {
-            document.getElementById("hide-hover-search").style.display = "none";
-        }
